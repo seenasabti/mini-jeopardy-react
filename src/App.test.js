4a56d797332808cd869db1da105b42c3d8374ec0
Seena Sabti <>
2020-01-29 14:29:43 -0500

    Corrects App.test.js to mock data fetch

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
+import Data from "./services/data";
 
-test('renders learn react link', () => {
-  const { getByTestId } = render(<App />);
+jest.mock("./services/data");
+
+beforeEach(()=>{
+  Data.mockImplementation(() => {
+    return {
+      get: async () => {
+        return await new Promise(resolve => {resolve({success: true, data: []})})
+      },
+    };
+  });
+})
+
+test('loads the app', () => {
+  const {getByTestId} = render(<App/>);
   const containerElement = getByTestId('app-container');
   expect(containerElement).toBeInTheDocument();
 });
