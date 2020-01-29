9c2642e2cd68c0c909effeb41d04bccc459bb481
Seena Sabti <>
2020-01-29 15:44:29 -0500

    Completes the answer component

--- src/components/answer/index.js
+++ src/components/answer/index.js
@@ -1,9 +1,65 @@
 import React, {Component} from "react";
 
 class Answer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.answer = props.answer.toLowerCase();
+    this.state = {value: '', correct: false, answered: false};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    let userAnswer = this.state.value.toLowerCase();
+    this.setState({
+      answered: true,
+      correct: this.answer === userAnswer
+    });
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+
+  correctAnswer() {
+    return (
+        <div className="text-center text-success p-4" data-testid='correct_answer'>
+          Correct Answer!
+        </div>
+    );
+  }
+
+  wrongAnswer() {
+    return (
+        <div className="text-center text-danger p-4" data-testid='wrong_answer'>
+          Wrong Answer :(
+        </div>
+    );
+  }
+
+  defaultAnswer() {
+    return (
+        <div className="input-group p-md-4 pb-4" data-testid='default_answer'>
+          <input type="text" className="form-control" placeholder="Type your answer..." value={this.state.value}
+                 onChange={this.handleChange} data-testid='input'/>
+          <div className="input-group-append">
+            <button className="btn btn-primary" onClick={this.handleClick}>Go!</button>
+          </div>
+        </div>
+    );
+  }
+
   render() {
-    return(<div></div>)
+    let items = this.defaultAnswer();
+    if (this.state.answered && this.state.correct) {
+      items = this.correctAnswer();
+    } else if (this.state.answered && !this.state.correct) {
+      items = this.wrongAnswer();
+    }
+    return items
   }
 }
 
-export default Answer;
+export default Answer
--- src/components/answer/index.test.js
+++ src/components/answer/index.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
 import Answer from './index';
 
 describe('successfully loads', () => {
   test('renders correctly', () => {
-    render(<Answer/>);
+    const {getByTestId, queryByTestId} = render(<Answer answer='answer 1' />);
+    const defaultAnswerElement = getByTestId('default_answer');
+    const wrongAnswerElement = queryByTestId('wrong_answer');
+    const correctAnswerElement = queryByTestId('correct_answer');
+
+
+    expect(defaultAnswerElement).toBeInTheDocument();
+    expect(wrongAnswerElement).toBe(null);
+    expect(correctAnswerElement).toBe(null);
+  });
+
+  test('user answers correctly', () => {
+    const {getByText, getByTestId} = render(<Answer answer='answer 1' />);
+    const input = getByTestId('input');
+
+    fireEvent.change(input, {target: { value: 'answer 1' }});
+    fireEvent.click(getByText('Go!'));
+
+    const wrongAnswerElement = getByTestId('correct_answer');
+    expect(wrongAnswerElement).toBeInTheDocument();
+  });
+
+  test('user answers wrongly', () => {
+    const {getByText, getByTestId} = render(<Answer answer='answer 1' />);
+    const input = getByTestId('input');
+
+    fireEvent.change(input, {target: { value: 'wrong answer' }});
+    fireEvent.click(getByText('Go!'));
+
+    const wrongAnswerElement = getByTestId('wrong_answer');
+    expect(wrongAnswerElement).toBeInTheDocument();
   });
 });
