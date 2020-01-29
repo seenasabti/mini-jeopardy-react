8f0d3a58b56f93fa2aac01950ba548d90ef4a145
Seena Sabti <>
2020-01-29 15:09:53 -0500

    Completes the clue component

--- src/components/clue/index.js
+++ src/components/clue/index.js
@@ -1,13 +1,59 @@
 import React, {Component} from "react";
+import './index.css'
+import Answer from "../answer";
 
 class Clue extends Component {
+
+  constructor(props) {
+    super(props);
+    this.question = props.question;
+    this.value = props.value;
+    this.answer = props.answer;
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      toggled: false
+    }
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({
+      toggled: true
+    });
+  }
+
+  cssClasses() {
+    let classes = {
+      card: 'card bg-primary',
+      value: 'card-body front text-center',
+      question: 'card-body back text-center hidden',
+      answer: 'hidden'
+    };
+    if (this.state.toggled) {
+      classes = {
+        card: 'card bg-warning',
+        value: 'card-body front text-center hidden',
+        question: 'card-body back text-center',
+        answer: ''
+      };
+    }
+    return classes;
+  }
+
   render() {
-    return(
-        <div data-testid='clue'>
-          <p data-testid='clue-value'>100</p>
+    let classes = this.cssClasses();
+    return (
+        <div className={classes.card} onClick={this.handleClick} data-testid='clue'>
+          <p className={classes.value} data-testid='clue-value'>{this.value}</p>
+          <p className={classes.question} data-testid='clue-question'>{this.question}</p>
+          <span className={classes.answer} data-testid='clue-answer'>
+            <Answer answer={this.props.answer}/>
+          </span>
         </div>
     )
   }
+
 }
 
 export default Clue;
--- src/components/clue/index.test.js
+++ src/components/clue/index.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
 import Clue from './index';
 
 describe('successfully loads', () => {
   test('renders correctly', () => {
-    render(<Clue/>);
+    const {getByText} = render(<Clue answer='answer 1' value='100' question='question 1' />);
+    const questionElement = getByText('question 1');
+    const valueElement = getByText('100');
+
+    expect(questionElement).toBeInTheDocument();
+    expect(valueElement).toBeInTheDocument();
+  });
+
+  test('user clicks on clue', () => {
+    const {getByText, getByTestId} = render(<Clue answer='answer 1' value='100' question='question 1' />);
+
+    expect(getByText('question 1')).toHaveClass('hidden');
+    expect(getByText('100')).not.toHaveClass('hidden');
+    expect(getByTestId('clue-answer')).toHaveClass('hidden');
+
+    fireEvent.click(getByText('100'));
+
+    expect(getByText('question 1')).not.toHaveClass('hidden');
+    expect(getByTestId('clue-answer')).not.toHaveClass('hidden');
+    expect(getByText('100')).toHaveClass('hidden');
   });
 });
